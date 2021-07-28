fed192ba6e33d574fdb5aefc6c67beb6ba9b1cc0
Cepraga Mihail <yahim09@gmail.com>
2021-07-28 17:30:39 +0300

    install mongoosejs

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express")
+const mongoose = require('mongoose');
 
 const app = express()
+
+mongoose.connect("mongodb://myuser:mypassword@mongo:27017/?authSource=admin")
+.then(() => console.log("succesfully connected to DB"))
+.catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
     res.send("<h2>Hi There!</h2>")
